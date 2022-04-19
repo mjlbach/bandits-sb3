5d50e924df19a98438bf4c95b2ee06ea27052915
Michael Lingelbach <m.j.lbach@gmail.com>
2022-04-19 12:18:52 -0700

    Update

--- eval.py
+++ eval.py
@@ -7,21 +7,34 @@ from stable_baselines3.common.vec_env.vec_monitor import VecMonitor
 from bandit.env import DebuggingEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
-def main():
+def main(args):
     """
     Example to set a training process with Stable Baselines 3
     Loads a scene and starts the training process for a navigation task with images using PPO
     Saves the checkpoint and loads it again
     """
     # Obtain the arguments/parameters for the policy and create the PPO model
-    model = PPO.load("save/ckpt-9000")
+    model = PPO.load(args.path)
     eval_env = DebuggingEnv(debug=True)
 
+    successes = 0
+    total = 1000
     # Train the model for the given number of steps
-    for i in range(100):
-        mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=20)
-        print(mean_reward)
+    for _ in range(total):
+        mean_reward, _ = evaluate_policy(model, eval_env, n_eval_episodes=20)
+        if mean_reward == 1:
+            successes += 1
+    print(f"Success rate: {successes/total}")
 
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--path",
+        "-p",
+        required=True,
+        help="which config file to use [default: use yaml files in examples/configs]",
+    )
+    args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
-    main()
+    main(args)
--- train.py
+++ train.py
@@ -2,86 +2,13 @@ import logging
 import os
 from typing import Callable
 
-import torch as th
-import torch.nn as nn
 from stable_baselines3.ppo.ppo import PPO
-from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.utils import set_random_seed
 from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
 from stable_baselines3.common.vec_env.vec_monitor import VecMonitor
-from bandit.env import DebuggingEnv
+from bandit.env import make_env
 
-from gym.spaces import Dict
-
-
-class CustomCombinedExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space: Dict):
-        # We do not know features-dim here before going over all the items,
-        # so put something dummy for now. PyTorch requires calling
-        # nn.Module.__init__ before adding modules
-        super(CustomCombinedExtractor, self).__init__(observation_space, features_dim=1)
-
-        extractors = {}
-
-        total_concat_size = 0
-        feature_size = 128
-        for key, subspace in observation_space.spaces.items():
-            if key in ["vectorized_goal", "task_obs"]:
-                extractors[key] = nn.Sequential(nn.Linear(subspace.shape[0], feature_size), nn.ReLU())
-            elif key in ["rgb"]:
-                n_input_channels = subspace.shape[2]  # channel last
-                cnn = nn.Sequential(
-                    nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
-                    nn.ReLU(),
-                    nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
-                    nn.ReLU(),
-                    nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
-                    nn.ReLU(),
-                    nn.Flatten(),
-                )
-                test_tensor = th.zeros([subspace.shape[2], subspace.shape[0], subspace.shape[1]])
-                with th.no_grad():
-                    n_flatten = cnn(test_tensor[None]).shape[1]
-                fc = nn.Sequential(nn.Linear(n_flatten, feature_size), nn.ReLU())
-                extractors[key] = nn.Sequential(cnn, fc)
-            elif key in ["scan"]:
-                n_input_channels = subspace.shape[1]  # channel last
-                cnn = nn.Sequential(
-                    nn.Conv1d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
-                    nn.ReLU(),
-                    nn.Conv1d(32, 64, kernel_size=4, stride=2, padding=0),
-                    nn.ReLU(),
-                    nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=0),
-                    nn.ReLU(),
-                    nn.Flatten(),
-                )
-                test_tensor = th.zeros([subspace.shape[1], subspace.shape[0]])
-                with th.no_grad():
-                    n_flatten = cnn(test_tensor[None]).shape[1]
-                fc = nn.Sequential(nn.Linear(n_flatten, feature_size), nn.ReLU())
-                extractors[key] = nn.Sequential(cnn, fc)
-            else:
-                raise ValueError("Unknown observation key: %s" % key)
-            total_concat_size += feature_size
-
-        self.extractors = nn.ModuleDict(extractors)
-
-        # Update the features dim manually
-        self._features_dim = total_concat_size
-
-    def forward(self, observations) -> th.Tensor:
-        encoded_tensor_list = []
-
-        # self.extractors contain nn.Modules that do all the processing.
-        for key, extractor in self.extractors.items():
-            if key in ["rgb", "highlight", "depth", "seg", "ins_seg"]:
-                observations[key] = observations[key].permute((0, 3, 1, 2)) #type: ignore
-            elif key in ["scan"]:
-                observations[key] = observations[key].permute((0, 2, 1)) #type: ignore
-            encoded_tensor_list.append(extractor(observations[key])) #type: ignore
-        # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
-        return th.cat(encoded_tensor_list, dim=1)
-
+from bandit.model import CustomCombinedExtractor
 
 def main():
     """
@@ -91,16 +18,6 @@ def main():
     """
     tensorboard_log_dir = "log_dir"
     num_environments = 8
-
-    # Function callback to create environments
-    def make_env(rank: int, seed: int = 0) -> Callable:
-        def _init():
-            env = DebuggingEnv()
-            env.seed(seed + rank)
-            return env
-
-        set_random_seed(seed)
-        return _init
 
     # Multiprocess
     env = SubprocVecEnv([make_env(i) for i in range(num_environments)])
